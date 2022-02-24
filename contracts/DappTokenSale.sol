460c6946fa906de37e13f419da3f8f9617adc229
gkvpp1876 <gkvpp1876@gmail.com>
2022-02-24 23:02:58 +0530

    token sale ICO, buytokens and tracking tokens sold

--- contracts/DappTokenSale.sol
+++ contracts/DappTokenSale.sol
@@ -6,6 +6,9 @@ contract DappTokenSale {
     address admin; //state variable data is going to write on the blockchain, variable is not required to be exposed so we haven't defined public
     DappToken public tokenContract; //creating variable with DappToken
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
 
     constructor(DappToken _tokenContract, uint256 _tokenPrice) {
         //Assign an admin
@@ -14,5 +17,24 @@ contract DappTokenSale {
         tokenContract = _tokenContract;
         //Token Price
         tokenPrice = _tokenPrice;
+    }
+
+    //multiply function, internal -- is only accessable to the current contract instance or current address and the contracts derived from the currecnt contract
+    //pure return a value that is calculated based on given parameters and global memory, but cannot affect the value of any other global variable
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    //Buy Tokens
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        //Require that value is equal to tokens
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        //Require that the contract has enough toekns
+
+        //Require that a transfer is successful
+        //Keep track of tokenSold
+        tokensSold += _numberOfTokens;
+        //Trigger sell Event
+        emit Sell(msg.sender, _numberOfTokens);
     }
 }
--- test/DappTokenSale.js
+++ test/DappTokenSale.js
@@ -3,8 +3,11 @@ var DappTokenSale = artifacts.require('./DappTokenSale.sol');
 
 contract('DappTokenSale', function (accounts) {
     var tokenSaleInstance;
+    var buyer = accounts[1];
     var tokenPrice = 1000000000000000; //1 Wei smallest unit of ether!
-    
+    // var tokenPrice = 10000000000000000
+    var numberOfTokens;
+
     it('intialize the contract with the correct values', function () {
         return DappTokenSale.deployed().then(function (instance) {
             tokenSaleInstance = instance;
@@ -16,7 +19,33 @@ contract('DappTokenSale', function (accounts) {
             assert.notEqual(address, 0x0, 'has token contract address');
             return tokenSaleInstance.tokenPrice();
         }).then(function (price) { //token price is going to be 0.001 ether so stored Wei and validating with it
-            assert.equal(price, tokenPrice, 'token price is correct');
+            assert.equal(price.toNumber(), tokenPrice, 'token price is correct');
+        });
+    });
+
+    it('facilitates token buying', function () {
+        return DappTokenSale.deployed().then(function (instance) {
+            tokenSaleInstance = instance;
+            numberOfTokens = 10;
+            // var value = numberOfTokens * tokenPrice;
+            //to buy token we need number of tokens, value of the tokens, and the buyer
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice })
+        }).then(function (receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+            assert.equal(receipt.logs[0]._args._buyer, buyer, 'logs the account that purchased the tokens');
+            assert.equal(receipt.logs[0]._args._amount, numberOfTokens, 'logs the number of tokens purchased');
+            //getting the tokens sold
+            return tokenSaleInstance.tokensSold();
+        }).then(function (amount) {
+            assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+            //Try to buy the tokens different from the ether value
+            console.log("buyer data ",buyer);
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1 });
+        }).then(assert.fail).catch(function (error) {
+            // console.log(numberOfTokens * tokenPrice, error.message)
+            // assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+
         });
     });
 });
